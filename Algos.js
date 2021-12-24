0d2a69556b6ad52a89b822ca18a41a870e761203
JavascriptWithDon <donlevans29@gmail.com>
2021-12-24 03:40:41 +0000

    Added fizzBuzz Function that returns FizzBuzz if divisible by 15, fizz if by 3, buzz if by 5.

---
+++ Algos.js
@@ -0,0 +1,12 @@
+//FizBuzz Aglo
+
+function fizzBuzz (num) {
+  for(i = 1; i < num; i++) {
+    if(i % 15 === 0) console.log ("FizBuzz!");
+    else if (i % 5 === 0 ) console.log ("Buzz!");
+    else if (i % 3 === 0) console.log ("Fizz!");
+    else console.log(i);
+  }
+}
+
+fizzBuzz(20);
